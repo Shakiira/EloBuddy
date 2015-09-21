2d848f6e50961757c86c8c4497acfb396bd7171e
Shakiira <miracle2207@hotmail.com>
2015-09-21 14:13:59 +0200

    EloBuddy

---
+++ MuteAll/MuteAll/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace JustMute
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Game_OnStart;
+        }
+
+        private static void Game_OnStart(EventArgs args)
+        {
+            Chat.Say("/mute all");
+
+        }
+    }
+}
